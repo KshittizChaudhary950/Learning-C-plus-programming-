bd965928c5b458f4c0d6bb364c622c648833b71b
kshittiz <kshittizchaudhary950@gmail.com>
2021-09-24 16:35:38 +0545

    Learning C++ some basic programs[C

---
+++ 1.cpp
@@ -0,0 +1,63 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	// 1.program to show number enter by user
+	/*int a;
+	cout<<"Enter any number:: \n";
+	cin>>a;
+	cout<<"The enter number is:"<<a;
+	return 0;
+	*/
+	
+	// 2. program to print addtion of two number
+/*	int a,b,c;
+	cout<<"Enter First number \n";
+	cin>>a;
+	cout<<"Enter second number \n";
+	cin>>b;
+	c=a+b;
+	cout<<"The addition of two number is: \n"<<c<<"\n";
+	
+	cout<<a<<"+"<<b<<"="<<c;
+	return 0;
+	*/
+	
+ // 3.program to find Quotient and remainder 
+ /*int divisor, dividend, remainder, Quotient;
+ cout<<"Enter the value of dividend: \n";
+ cin>>dividend;
+ cout<<"Enter the value of divisor: \n";
+ cin>>divisor;
+ Quotient=dividend/divisor;
+ cout<<"Quotient is:\n"<<Quotient<<"\n";
+ remainder=dividend%divisor;
+ cout<<"Remainder is: \n"<<remainder;
+ */
+ 
+ // 4. Program to swap two number without using third variable
+ /* Algorithms 
+ 1. declare variable
+ 2. take input from user and store in the variable
+ 3. show enter number before swaping
+ 4. a-a+b;
+  5.  b- a-b;
+  6.  a- a-b;
+  7. show swaped number to user   
+ /*
+ int a, b;
+ cout<<"Enter First number:"<<endl;
+ cin>>a;
+ cout<<"Enter second number:"<<endl;
+ cin>>b;
+ cout<<"Before swaping two number are:\n"<<a<<"\n"<<b<<endl;
+ a=a+b;
+ b=a-b;
+ a=a-b;
+cout<<"After swaping two number are:\n "<<a<<"\n"<<b<<endl;
+return 0;
+*/
+ 
+};
+
+
