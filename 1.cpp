6bb0652f63ea0b4e80d8f384421b3d2510434150
kshittiz <kshittizchaudhary950@gmail.com>
2021-09-24 16:43:59 +0545

    nothing new is added

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main()
 {
 	// 1.program to show number enter by user
-	/*int a;
+	
+	/*
+	-------------------------------------------------------------------
+	int a;
 	cout<<"Enter any number:: \n";
 	cin>>a;
 	cout<<"The enter number is:"<<a;
@@ -11,7 +14,10 @@ int main()
 	*/
 	
 	// 2. program to print addtion of two number
-/*	int a,b,c;
+	
+/*	
+	---------------------------------------------------------------------
+	int a,b,c;
 	cout<<"Enter First number \n";
 	cin>>a;
 	cout<<"Enter second number \n";
@@ -24,7 +30,10 @@ int main()
 	*/
 	
  // 3.program to find Quotient and remainder 
- /*int divisor, dividend, remainder, Quotient;
+ 
+ /*
+ ----------------------------------------------------------------------------
+ int divisor, dividend, remainder, Quotient;
  cout<<"Enter the value of dividend: \n";
  cin>>dividend;
  cout<<"Enter the value of divisor: \n";
@@ -36,14 +45,17 @@ int main()
  */
  
  // 4. Program to swap two number without using third variable
+ //-----------------------------------------------------------------//
  /* Algorithms 
+ 
  1. declare variable
  2. take input from user and store in the variable
  3. show enter number before swaping
  4. a-a+b;
   5.  b- a-b;
   6.  a- a-b;
-  7. show swaped number to user   
+  7. show swaped number to user  
+  --------------------------------------------------------------------- 
  /*
  int a, b;
  cout<<"Enter First number:"<<endl;
