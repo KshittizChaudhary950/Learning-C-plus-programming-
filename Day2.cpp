11aea2a986f438d629c1fac49b0c005db5a6e27e
kshittiz <kshittizchaudhary950@gmail.com>
2021-09-26 19:17:59 +0545

    ASCII program

--- Day2.cpp
+++ Day2.cpp
@@ -4,10 +4,34 @@ int main()
 {
 	// program to find size of different datatypes 
   	//----------------------------------------------------------//
+  	/*
   	cout<<"The size of int is:"<<sizeof(int)<<"bytes"<<endl;
   	cout<<"The size of char is:"<<sizeof(char)<<"bytes"<<endl;
   	cout<<"The size of float is:"<<sizeof(float)<<"bytes"<<endl;
   	cout<<"The size of double is:"<<sizeof(double)<<"bytes"<<endl;
+  	*/
+  	
+  	//C++ Program to Find ASCII Value of a Character
+  	/*
+  	--------------------------------------------------------------
+  	algorithms 
+  	1. Start 
+  	2. declare variable of char data type
+  	3. take input from user 
+  	4. assign value to variable
+  	5. print ASCII value of that char
+  	6. End
+  	Note: Upercase and lowercase letter have different value
+  	------------------------------------------------------------------
+  	*/
+  	/*
+  	char C;
+  	cout<<"Enter any character type ??\n";
+  	cin>>C;
+  	cout<<"The ASCII value of ["<<C<<"] is:"<<int(C)<<endl; 
+  	*/
+  	
+  	
 	
 	return 0;
 }
