f2b46e7cb89cfd8ca5bfe162753559cdd62e6404
kshittiz <kshittizchaudhary950@gmail.com>
2021-09-26 19:00:44 +0545

    day2

---
+++ Day2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// program to find size of different datatypes 
+  	//----------------------------------------------------------//
+  	cout<<"The size of int is:"<<sizeof(int)<<"bytes"<<endl;
+  	cout<<"The size of char is:"<<sizeof(char)<<"bytes"<<endl;
+  	cout<<"The size of float is:"<<sizeof(float)<<"bytes"<<endl;
+  	cout<<"The size of double is:"<<sizeof(double)<<"bytes"<<endl;
+	
+	return 0;
+}
