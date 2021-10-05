58f5cd82ca60e6daf6ee903b8fa8dd8e2c43cb02
kshittiz <kshittizchaudhary950@gmail.com>
2021-10-05 15:23:59 +0545

    use of macros

--- Programs.cpp
+++ Programs.cpp
@@ -34,16 +34,34 @@ int main()
 	program to print square root of any number using sqrt() function
 	
 */
-
+/*
 #include <iostream>
 #include <math.h>
 using namespace std;
 int main()
 {
-	int num,sqt;
+	float num,sqt;
 	cout<<"Enter the number to square root \n";
 	cin>>num;
-	sqt=sqrt(num);
+	sqt=sqrt(num);//using sqrt function which is define in math.h header file
 	cout<<"The square root of "<<num<<" is "<<sqt<<endl;
 	return 0;
 }
+*/
+//program to print cube of a number using macros
+// macro is define in the header file #define 
+//------------------------------------------------------------------------------//
+#include <iostream>
+#include <math.h>
+#define cube(base) pow(base,3)
+using namespace std;
+int main()
+{
+	float num=2,cube;
+	cube=cube(num);
+	cout<<"The cube of: ["<<num<<"] is "<<cube<<endl;
+	
+	return 0;
+}
+
+//----------------------------------------------------------------------------------//
